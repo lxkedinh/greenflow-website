b2ade19ac19d6e996edd20bf4768cc4170e77754
Luke Dinh <lukexdinh@gmail.com>
2022-05-24 08:25:46 -0700

    added input sanitization

--- functions/routes/formsubmission.js
+++ functions/routes/formsubmission.js
@@ -1,56 +1,62 @@
 let express = require("express");
 const functions = require("firebase-functions");
+const { body } = require("express-validator");
 let router = express.Router();
 require("dotenv").config();
 
 // MailJet API Keys
 const mailjet = require("node-mailjet").connect(
-  // process.env.MAILJET_PUBLIC,
-  // process.env.MAILJET_SECRET
-  `${functions.config().mailjetpublic.key}`,
-  `${functions.config().mailjetsecret.key}`
+  process.env.MAILJET_PUBLIC,
+  process.env.MAILJET_SECRET
 );
 
 // POST form submission page.
-router.post("/", function (req, res, next) {
-  /**
-   * Send an e-mail message to ourselves with name, contact info, and message of user who submitted HTML form on website
-   */
-  const request = mailjet.post("send", { version: "v3.1" }).request({
-    Messages: [
-      {
-        /**
-         * TODO: replace with official greenflowhvac email later after Namecheap email is set up
-         */
-        From: {
-          Email: "lukexdinh@gmail.com",
-          Name: `${req.body.firstName} ${req.body.lastName}`,
-        },
-        To: [
-          {
-            Email: "lukexdinh@gmail.com",
-            Name: "Greenflow HVAC Website",
+router.post(
+  "/",
+  [
+    body("firstName").trim().escape(),
+    body("lastName").trim().escape(),
+    body("phone").trim().escape(),
+    body("email").trim().isEmail().normalizeEmail().escape(),
+    body("message").trim().escape(),
+  ],
+  function (req, res) {
+    /**
+     * Send an e-mail message with name, contact info, and message of user who submitted HTML form on website
+     */
+    console.log(req.body);
+
+    const request = mailjet.post("send", { version: "v3.1" }).request({
+      Messages: [
+        {
+          From: {
+            Email: process.env.EMAIL,
+            Name: `${req.body.firstName} ${req.body.lastName}`,
           },
-        ],
-        Subject: "Greenflow HVAC Contact Form Request",
-        TextPart: `From ${req.body.firstName} ${req.body.lastName} at ${req.body.email} or ${req.body.phone},
+          To: [
+            {
+              Email: process.env.EMAIL,
+              Name: "Greenflow HVAC Website",
+            },
+          ],
+          Subject: "Greenflow HVAC Contact Form Request",
+          TextPart: `From ${req.body.firstName} ${req.body.lastName} at ${req.body.email} or ${req.body.phone},
           ${req.body.message}`,
-        HTMLPart: `Greenflow HVAC Contact Form Request</h3>
+          HTMLPart: `Greenflow HVAC Contact Form Request</h3>
             <p>From ${req.body.firstName} ${req.body.lastName} at ${req.body.email} or ${req.body.phone},</p><br /><br />
             <p>${req.body.message}`,
-      },
-    ],
-  });
-
-  request
-    .then((result) => {
-      // console.log(result.body);
-      res.redirect("/form-success");
-    })
-    .catch((err) => {
-      // console.log(err);
-      res.redirect("/form-failure");
+        },
+      ],
     });
-});
+
+    request
+      .then(() => {
+        res.redirect("/form-success");
+      })
+      .catch(() => {
+        res.redirect("/form-failure");
+      });
+  }
+);
 
 module.exports = router;
